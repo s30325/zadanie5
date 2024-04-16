6bf5b2c73b0e24d97aff34623f4c0e5d012d7a57
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-16 14:16:17 +0200

    Update Goal.java

--- zadanie5/src/Goal.java
+++ zadanie5/src/Goal.java
@@ -1,2 +1,23 @@
 public class Goal {
-}
+    public int year;
+    public int dayNumber;
+    public int monthNumber;
+    public String name;
+    public int bonus;
+
+    public Goal(int year, int dayNumber, int monthNumber, String name, int bonus) {
+        this.year = year;
+        this.dayNumber = dayNumber;
+        this.monthNumber = monthNumber;
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public int zwrocBonus() {
+        return bonus;}
+        public int zwrocRok() {
+            return year;}
+            public int zwrocMiesiac() {
+                return monthNumber;}
+            }
+
