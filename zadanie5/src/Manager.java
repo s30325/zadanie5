a95f4a5e769e82ee7189497301fa1688471aec94
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-16 14:17:07 +0200

    Update Manager.java

--- zadanie5/src/Manager.java
+++ zadanie5/src/Manager.java
@@ -1,2 +1,25 @@
-public class Manager {
+import java.util.ArrayList;
+import java.util.Calendar;
+
+class Manager extends Employee {
+    public Manager(String firstName, String lastName, String adress, String email, String pesel, int employeeYear) {
+        super(firstName, lastName, adress, email, pesel, employeeYear);
+    }
+    public ArrayList<Goal> listaCelow = new ArrayList<>();
+    public void addGoals(Goal goal){
+        listaCelow.add(goal);
+    }
+    @Override
+    public int calculateSalary(){
+        int basicSalary = super.calculateSalary();
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        for(Goal goal : listaCelow){
+            if(goal.zwrocRok() == year && goal.zwrocMiesiac() == month){
+                basicSalary += goal.zwrocBonus();
+            }
+        }
+        return basicSalary;
+    }
 }
