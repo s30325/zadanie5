4f11390588aea99d1c9896482b82d06121a004b1
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-16 14:17:24 +0200

    Update Technology.java

--- zadanie5/src/Technology.java
+++ zadanie5/src/Technology.java
@@ -1,8 +1,11 @@
 public class Technology {
     public String nameOfTechnology;
-    public String bonusMoney;
-    public Technology(String nameOfTechnology,String bonusMoney){
+    public int bonusMoney;
+    public Technology(String nameOfTechnology,int bonusMoney){
         this.nameOfTechnology = nameOfTechnology;
         this.bonusMoney = bonusMoney;
+    }
+    public int zwrocBonus(){
+        return bonusMoney;
     }
 }
