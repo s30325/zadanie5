8228ef860dd67c0108a5220fafb287963486d3ee
s30325 <162446731+s30325@users.noreply.github.com>
2024-04-16 14:17:48 +0200

    Update Tester.java

--- zadanie5/src/Tester.java
+++ zadanie5/src/Tester.java
@@ -1,2 +1,17 @@
-public class Tester {
+import java.util.ArrayList;
+
+class Tester extends Employee {
+    public Tester(String firstName, String lastName, String adress, String email, String pesel, int employeeYear) {
+        super(firstName, lastName, adress, email, pesel, employeeYear);
+    }
+    public ArrayList<String> TestType = new ArrayList<>();
+    public void addTestType(String testType){
+        TestType.add(testType);
+    }
+    @Override
+    public int calculateSalary() {
+        int basicSalary = super.calculateSalary();
+        basicSalary +=TestType.size()*300;
+        return basicSalary;
+    }
 }
